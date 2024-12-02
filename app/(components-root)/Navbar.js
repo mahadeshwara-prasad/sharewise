5cb31cb20f63b31ec373babf7565c3d40c91baff
mahadeshwara-prasad <mahadeshwara.prasad07@gmail.com>
2024-12-03 01:34:14 +0530

    Ready to Deploy

--- app/(components-root)/Navbar.js
+++ app/(components-root)/Navbar.js
@@ -5,12 +5,9 @@ import Link from 'next/link';
 import Notification from './Notification';
 import { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
-import { useSignMessage } from 'wagmi'
 
 export default function Nav(){
-    const verifyUser = useSignMessage({
-        message: 'gm wagmi frens',
-      })
+
     const {address} = useAccount();
     const [verified,setVerified] = useState(false);
     const fetchData = async () => {
@@ -116,20 +113,6 @@ export default function Nav(){
                     <li className='pt-2'>
                         <ConnectButton></ConnectButton>
                     </li>
-                    {verified?
-                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-blue-600">
-                            <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
-                        </svg>
-                        <h4 className='text-blue-500'>Verified</h4>
-                    </a>:
-                    <div className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-red-600">
-                        <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                    </svg>
-                    
-                    <button className='text-red-600 bg-color-white' onClick={()=>{verifyUser?.signMessage();}}> Not Verified</button>
-                </div>}
                 </ul>
             </div>
             </div>
